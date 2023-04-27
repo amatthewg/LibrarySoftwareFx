b4d944d4b1200dd4d30a467811b2064964b3394c
amg <58791674+amatthewg@users.noreply.github.com>
2023-04-26 20:51:34 -0400

    Test Commit

--- src/main/java/com/example/librarysoftwarefx/Main.java
+++ src/main/java/com/example/librarysoftwarefx/Main.java
@@ -6,15 +6,23 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, URISyntaxException {
         Stage primaryStage = new Stage();
         primaryStage.show();
+
+        String jarPath = Main.class.getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .toURI()
+                .getPath();
+        System.out.println(jarPath);
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
